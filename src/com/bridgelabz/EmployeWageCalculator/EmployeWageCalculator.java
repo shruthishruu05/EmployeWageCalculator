d3d363cfd3e3173d271f567cc44f501894cccb20
shruthishruu05 <shruthi.m@perfios.com>
2021-08-30 23:39:06 +0530

    ADDED :both full time and part time wage

--- src/com/bridgelabz/EmployeWageCalculator/EmployeWageCalculator.java
+++ src/com/bridgelabz/EmployeWageCalculator/EmployeWageCalculator.java
@@ -1,24 +1,28 @@
 package com.bridgelabz.EmployeWageCalculator;
 
 public class EmployeWageCalculator {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	
 	public static void main(String[] args) {
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		double empCheck = Math.floor(Math.random()*10) % 3;
-		if( empCheck == IS_FULL_TIME) {
-			empHrs = 8;
-		}
-		else if(empCheck == IS_PART_TIME) {
+		int empCheck = (int) Math.floor(Math.random()*10) % 3;
+		switch (empCheck) {
+		case IS_PART_TIME : 
 			empHrs = 4;
-		}
-		else {
+			break;
+		case IS_FULL_TIME :
+			empHrs = 8;
+			break;
+		default :
 			empHrs = 0;
 		}
+		
 		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("EmpLoyee Wage : "+ empWage);
+		System.out.println("Emp Wage : "+ empWage);
 	}
+
 
 }
