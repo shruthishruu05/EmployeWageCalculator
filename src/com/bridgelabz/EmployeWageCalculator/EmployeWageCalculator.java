3e81b3cab23e81cc9e36217d6db6942633f2f64d
shruthishruu05 <shruthi.m@perfios.com>
2021-08-30 23:48:31 +0530

    ADDED :maximum working hour and days condition

--- src/com/bridgelabz/EmployeWageCalculator/EmployeWageCalculator.java
+++ src/com/bridgelabz/EmployeWageCalculator/EmployeWageCalculator.java
@@ -4,13 +4,13 @@ public class EmployeWageCalculator {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HOURS_IN_MONTH = 100;
 	
 	public static void main(String[] args) {
-		//variables
-		int empHrs = 0, empWage = 0, totalEmpWage =0;
-		//compuation
-		for(int day=0;day< NUM_OF_WORKING_DAYS;day++) {
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays =0;
+		while(totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays ++;
 			int empCheck = (int) Math.floor(Math.random()*10) % 3;
 			switch (empCheck) {
 			case IS_PART_TIME : 
@@ -23,12 +23,13 @@ public class EmployeWageCalculator {
 				empHrs = 0;
 			}
 		
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage : "+ empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Days#: "+totalWorkingDays+"     Emp Hrs: "+ empHrs);
 		}
-		System.out.println("Total Emp Wage: " + totalEmpWage);
+		int totalWage = totalEmpHrs *totalWorkingDays;
+		System.out.println("Total Emp Wage: " + totalWage );
 	}
+
 }
 
 
