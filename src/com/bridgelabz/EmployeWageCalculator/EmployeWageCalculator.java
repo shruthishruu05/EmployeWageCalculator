49a09b772b5afd12dafada2f6f9c0819a3f16d05
shruthishruu05 <shruthi.m@perfios.com>
2021-08-30 22:29:36 +0530

    ADDED :Daily wage calculator

--- src/com/bridgelabz/EmployeWageCalculator/EmployeWageCalculator.java
+++ src/com/bridgelabz/EmployeWageCalculator/EmployeWageCalculator.java
@@ -14,7 +14,7 @@ public class EmployeWageCalculator {
 			empHrs = 0;
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage : "+ empWage);
+		System.out.println("Employe Wage : "+ empWage);
 	}
 
 }
